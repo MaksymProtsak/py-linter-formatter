2a63fe6e6c0755b753440527e8d3842c156d018d
Maksym Protsak <maksym.protsak@gmail.com>
2024-05-10 08:27:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -10,34 +10,13 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
-            for error in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed"
+        "status": "passed" if errors == [] else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {"errors": [
-            {
-                "line": error_descr["line_number"],
-                "column": error_descr["column_number"],
-                "message": error_descr["text"],
-                "name": error_descr["code"],
-                "source": "flake8",
-            }
-            for error_descr in linter_report[source_code_key]]
-            if linter_report[source_code_key] != [] else [],
-         "path": source_code_key,
-         "status": "passed" if linter_report[source_code_key] == []
-         else "failed"}
-        for source_code_key in linter_report
-    ]
+    return [format_single_linter_file(source_code_key,
+            linter_report[source_code_key])
+            for source_code_key in linter_report]
